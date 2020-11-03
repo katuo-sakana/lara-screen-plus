fa9ab6f36ea9c32a58e85f9006cd9864d5ed9427
katuo-sakana <katuo.webkaihatsu@gmail.com>
2020-11-03 19:43:21 +0900

    puphpeteerをdocker環境で動かせるため。laravelwebsocketsは不必要かも

--- backend/app/Http/Controllers/HomeController.php
+++ backend/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Nesk\Puphpeteer\Puppeteer;
 
 class HomeController extends Controller
 {
@@ -14,6 +15,19 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $puppeteer = new Puppeteer;
+        // $browser = $puppeteer->launch();
+        $browser = $puppeteer->launch([
+            'executablePath' => env('CHROMIUM_PATH'),
+            'args' => ['--no-sandbox', '--disable-setuid-sandbox']
+            // 'executablePath' => '/path/to/Chrome'
+        ]);
+
+        $page = $browser->newPage();
+        $page->goto('https://www.s-oj.com/ec-business/system/php-puppeteer-puphpeteer/');
+        $page->screenshot(['path' => 'example04.png']);
+
+        $browser->close();
         return view('welcome');
     }
 }
